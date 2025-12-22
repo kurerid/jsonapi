3ae956ea19aa916d5dc75ede8d59c0c02db496fc
vasnin10a22syu <kurerid@yandex.ru>
2025-12-22 13:00:12 +0500

    main обновляет assignValue

--- request.go
+++ request.go
@@ -656,13 +656,6 @@ func assign(field, value reflect.Value) {
 // assign assigns the specified value to the field,
 // expecting both values not to be pointer types.
 func assignValue(field, value reflect.Value) {
-	if !value.IsValid() || !value.CanInterface() {
-		// Если нельзя, устанавливаем zero value
-		fmt.Println(field.Type())
-		field.Set(reflect.New(field.Type()))
-		return
-	}
-
 	switch field.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16,
 		reflect.Int32, reflect.Int64:
